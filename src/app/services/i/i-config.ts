f1b8fd7dfd10ace624ef108a99a979f0a8656c2c
chris_dibbern <chrisdibbern@gmail.com>
2017-03-26 00:12:39 -0500

    Fixing export defs, config options.

--- src/app/services/i/i-config.ts
+++ src/app/services/i/i-config.ts
@@ -2,7 +2,7 @@ import { ILogService } from './i-log.service';
 
 export interface IConfig {
     /** This defaults to console. */
-    logger: ILogService;
+    logger?: ILogService;
 
     /** The dictionary of view models to use for recursive mapping. */
     viewModels: { [key: string]: any };
---
+++ src/app/simple-mapper.module.ts
@@ -0,0 +1,31 @@
+
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapperService, MapperConfiguration } from './services/mapper.service';
+import { IConfig } from './services/i';
+
+export * from './services/mapper.service';
+export * from './services/i';
+export * from './decorators/mappable.decorator';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    MapperService
+  ],
+  exports: [
+  ]
+})
+export class SimpleMapperModule {
+  static forRoot(config: IConfig): ModuleWithProviders {
+    return {
+      ngModule: SimpleMapperModule,
+      providers: [
+        MapperService,
+        { provide: MapperConfiguration, useValue: config }
+      ]
+    };
+  }
+}
