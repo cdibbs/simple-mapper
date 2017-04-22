b0e9b2cc2a90f5a2209ed69441cc75ea656b1737
chris_dibbern <chrisdibbern@gmail.com>
2017-04-22 14:14:08 -0500

    Updating dependencies, adding compodoc to gitignore

--- src/decorators/mappable.decorator.ts
+++ src/decorators/mappable.decorator.ts
@@ -9,15 +9,13 @@ export function mappable(type: decoratorInput)
         if (typeof type === "undefined") {
             throw new Error(`Provided type for property "${propertyKey}" on "${target.constructor.name}" is undefined. Did you define the type before using it?`);
         }
-        let d: { [propKey: string]: decoratorInput }
-            = Reflect.get(target, mapMetadataKey) || {};
+        let d: { [propKey: string]: decoratorInput } = Reflect.get(target, mapMetadataKey) || {};
         d[propertyKey] = type;
         Reflect.set(target, mapMetadataKey, d);
     };
 }
 
-export function getMappableProperties(target: Object)
-    : { [propKey : string]: decoratorInput }
+export function getMappableProperties(target: Object): { [propKey : string]: decoratorInput }
 {
     if (typeof target === 'function') {
         target = target['prototype'];
