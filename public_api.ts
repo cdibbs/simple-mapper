bc51fba4deecf2ad95fb00239d3ab72d5976a8e6
chris_dibbern <chrisdibbern@gmail.com>
2017-04-22 17:05:45 -0500

    Fixing exports and updating deps

--- public_api.ts
+++ public_api.ts
@@ -1,7 +1,4 @@
-export {
-    IConfig,
-    SimpleMapperModule,
-    MapperLoggerToken, ILogService,
-    mappable, decoratorInput, getMappableProperties,
-    MapperServiceToken, IMapperService, MapperService } from './src/index';
-
+export { SimpleMapperModule } from './src/modules/simple-mapper.module';
+export { MapperService, MapperServiceToken } from './src/services/mapper.service';
+export { MapperLoggerToken, IMapperService, ILogService, IConfig } from './src/services/i';
+export { getMappableProperties, mappable, decoratorInput } from './src/decorators/mappable.decorator';
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './modules/simple-mapper.module';
-export * from './services/mapper.service';
-export * from './services/i';
-export * from './decorators/mappable.decorator';
